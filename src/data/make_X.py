c099ddc7f0ba95c5a544f7681b74dbe9ed70f546
loganschmitt14 <loganschmitt14@gmail.com>
2023-11-20 11:29:41 -0500

    finished feature engineering

--- src/data/make_X.py
+++ src/data/make_X.py
@@ -4,18 +4,11 @@ import numpy as np
 import os
 from pathlib import Path
 
-
-database_path = os.path.abspath(
-            os.path.join(
-                os.pardir,
-                os.pardir,
-                'data',
-                'processed',
-                'books.db'
-                )
-            )
-
 def make_X_data(database_path, X_path):
+    '''(Path, Path) -> CSV
+    Accepts path to books.db database and desired output directory.
+    Saves a CSV to output directory with feature engineered data.
+    '''
     
     conn = sqlite3.connect(database_path)
     
@@ -25,4 +18,48 @@ def make_X_data(database_path, X_path):
 
     conn.close()
 
-    ratings_df['standardized_rating'] = ratings_df.groupby('user_id')['user_rating'].transform(standardize_ratings)
+    min_ratings = 99
+    
+    filter_users = ratings_df['user_id'].value_counts() >= min_ratings
+    
+    filter_users = filter_users[filter_users].index.to_list()
+    
+    filtered_ratings_df = ratings_df[ratings_df['user_id'].isin(filter_users)]
+
+    filtered_ratings_df.to_csv(X_path)
+
+
+if __name__ == '__main__':
+
+    database_path = os.path.abspath(
+            os.path.join(
+                os.pardir,
+                os.pardir,
+                'data',
+                'processed',
+                'books.db'
+                )
+            )
+
+
+    X_path = os.path.abspath(
+        os.path.join(
+            os.pardir,
+            os.pardir,
+            'data',
+            'processed',
+            'X.csv'
+            )
+        )
+
+
+    make_X_data(database_path, X_path)
+
+
+
+
+
+
+
+
+
